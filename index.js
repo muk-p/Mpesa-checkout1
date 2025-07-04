7af193fc6798ba5294ae3de21c6e7fea3ea8f33d
muk-p <jmukara959@gmail.com>
2025-07-04 16:36:35 +0300

    cors change

--- index.js
+++ index.js
@@ -1,9 +1,15 @@
 const express = require("express");
+const cors = require("cors");
 const axios = require("axios");
 const bodyParser = require("body-parser");
 require("dotenv").config();
 
 const app = express();
+
+app.use(cors({
+  origin: 'http://localhost:3000', 
+}));  
+
 app.use(bodyParser.json());
 
 const {
@@ -66,6 +72,6 @@ app.post("/callback", (req, res) => {
   res.status(200).json({ message: "Callback received" });
 });
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => console.log(`✅ Server running on port ${PORT}`));
 
